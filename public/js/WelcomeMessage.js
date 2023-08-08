7cd7c9df303f53e24a6151327032d0f645f41d18
KsaweryLes <klesniak@insolutions.pl>
2023-08-08 15:36:36 +0200

    working JS+html examples

--- public/js/WelcomeMessage.js
+++ public/js/WelcomeMessage.js
@@ -1,27 +1,23 @@
+//tutorial 2
 let myButton = confirm("Change the user");
-let myHeading = document.querySelector("h1");
-
-// myButton.onclick = () => {
-//     setUserName();
-// };
-
+let myHeading = document.getElementsByTagName("h1");
 if(myButton) {setUserName();}
 function setUserName() {
     const myName = prompt("Please enter your name.");
     localStorage.setItem("name", myName);
-    //myHeading.textContent = `Mozilla is cool, ${myName}`;     causes error
-    alert(`Mozilla is cool, ${myName}`);
+    document.getElementById("demo").innerHTML = `Mozilla is cool, ${myName}`;
 }
 
 if (!localStorage.getItem("name")) {
     setUserName();
 } else {
     const storedName = localStorage.getItem("name");
-    //myHeading.textContent = `Mozilla is cool, ${storedName}`;
+    document.getElementById("demo").innerHTML = `Mozilla is cool, ${storedName}`;
     alert(`Mozilla is cool, ${storedName}`);
 }
 
+//tutorial 3
 
+//const element = document.getElementsByTagName("p");
 
-//let myButton = document.querySelector("button");
-//let myHeading = document.querySelector("h1");
+//document.getElementById("demo").innerHTML = 'The text in first paragraph (index 0) is: ' + element[0].innerHTML;

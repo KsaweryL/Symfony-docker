eb9abef84eb6179d3c73b40e32f88bd13ddea21c
KsaweryLes <klesniak@insolutions.pl>
2023-08-11 15:54:16 +0200

    addec omment

--- public/js/user/userPanel.js
+++ public/js/user/userPanel.js
@@ -3,6 +3,7 @@ let checkbox =  document.querySelector("#accept");
 let myButton = confirm("Welcome back!");
 
 function isChecked(){
+        //to finish ...
         document.getElementById("taskStatus").innerHTML = "Yes";
 
 }
